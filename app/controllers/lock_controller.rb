183968d6fd87aa71b3c5ab0489cffa04afc1a723
Joanna Rajewska <joanna.rajewska@netguru.pl>
2013-04-17 21:26:46 +0200

    add lock app

--- app/controllers/lock_controller.rb
+++ app/controllers/lock_controller.rb
@@ -2,8 +2,14 @@ class LockController < ApplicationController
   def unlock
     if Lock.passwords_match?(params[:password])
       session[:lock_opened] = true
+      redirect_to root_path
     else
       redirect_to :action=>:login
     end
+  end
+
+  def lock
+    session.delete :lock_opened
+    redirect_to lock_login_path
   end
 end
