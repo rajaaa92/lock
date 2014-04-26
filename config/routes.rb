8e53dc65002b1db4b8709cfa6fb0df6553760a92
Joanna Rajewska <joanna.rajewska@netguru.pl>
2014-04-26 19:43:07 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get "lock/login", to: "lock#login", as: "lock_login"
   post "lock/unlock", to: "lock#unlock", as: "unlock"
-  post '/lock' => 'lock#lock'
+  get '/lock' => 'lock#lock'
 end
